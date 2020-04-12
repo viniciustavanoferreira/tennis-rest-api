422b403edf102d02358c2fd9feb11492dbbfb15a
allanneros <allan@neros.com.br>
2020-04-12 00:10:52 -0300

    v1.0 Implementação das funções para incluir e remover amigos

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -10,9 +10,70 @@ class User_model extends CI_Model {
         parent::__construct(); 
     }
 
+    //remove friend
+    public function removeFriend($user_id,$friend_user_id) {
+        //remove id1 -> id2
+        $data = array(
+            'user_id_1'   =>  $user_id,
+            'user_id_2'   =>  $friend_user_id
+        );
+        $this->db->where($data);
+        $this->db->delete('tennis_user_relation');
+        $del_1 = ($this->db->affected_rows()>0?TRUE:FALSE);
+
+        //remove id1 -> id2
+        $data = array(
+            'user_id_2'   =>  $user_id,
+            'user_id_1'   =>  $friend_user_id
+        );
+        $this->db->where($data);
+        $this->db->delete('tennis_user_relation');
+        $del_2 = ($this->db->affected_rows()>0?TRUE:FALSE);
+        
+        return ($del_1 || $del_2);
+    }
+    //add friend 
+    public function addFriend($user_id,$friend_user_id) {
+        $data = array(
+            'user_id_1'   =>  $user_id,
+            'user_id_2'   =>  $friend_user_id
+        );
+        $this->db->insert('tennis_user_relation',$data);
+        return ($this->db->affected_rows()>0?TRUE:FALSE);
+    }
+    //list friends where user is listed
+    public function getFriends($id) {
+        $this->db->from('uvw_user_friends');
+        $this->db->where(array('id'=>$id));
+        $query = $this->db->get();
+        return $query->result_array();
+    }
+
+    //remove club
+    public function removeClub($user_id,$club_id) {
+        $data = array(
+            'user_id'   =>  $user_id,
+            'club_id'   =>  $club_id
+        );
+        $this->db->delete('tennis_user_club',$data);
+        return ($this->db->affected_rows()>0?TRUE:FALSE);
+    }
+
+    //add club 
+    public function addClub($user_id,$club_id) {
+        $data = array(
+            'user_id'   =>  $user_id,
+            'club_id'   =>  $club_id
+        );
+        $this->db->insert('tennis_user_club',$data);
+        return ($this->db->affected_rows()>0?TRUE:FALSE);
+    }
+
     //list clubs where user is listed
     public function getClubs($id) {
-        $query = $this->db->get_where('uvw_user_clubs',array('id'=>$id));
+        $this->db->from('uvw_user_clubs');
+        $this->db->where(array('id'=>$id));
+        $query = $this->db->get();
         return $query->result_array();
     }
 
